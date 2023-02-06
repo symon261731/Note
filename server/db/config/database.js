be2d7232643d63d7e4244de465ee5bf68ffddaa5
Simon Mishchenko <symon261731@gmail.com>
2023-02-06 17:18:48 +0300

    add docker

--- server/db/config/database.js
+++ server/db/config/database.js
@@ -2,24 +2,25 @@ require('dotenv').config();
 
 module.exports = {
   development: {
-    username: process.env.DB__USER,
+    username: 'simon',
     password: '123',
-    database: process.env.DB_DB,
-    host: '127.0.0.1',
+    database: 'note',
+    // host: '127.0.0.1',
+    host: 'postgres',
     dialect: 'postgres',
   },
   test: {
     username: 'root',
     password: null,
     database: 'database_test',
-    host: '127.0.0.1',
+    host: 'postgres',
     dialect: 'mysql',
   },
   production: {
     username: 'root',
     password: null,
     database: 'database_production',
-    host: '127.0.0.1',
+    host: 'postgres',
     dialect: 'mysql',
   },
 };
